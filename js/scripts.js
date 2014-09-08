9de9389f5c413179ba8c1ef8229e49b9f165acc4
Ryan Bonhardt <rbonhardt@gmail.com>
2014-09-08 14:46:54 -0400

    typed js import complete

--- js/scripts.js
+++ js/scripts.js
@@ -142,3 +142,27 @@ $(window).load(function() {
 	});
 
 });
+
+/******** Typed JS ******/
+$(function(){
+
+        $("#typed").typed({
+            strings: ["Typed.js is a jQuery plugin.", "It types out sentences.", "And then deletes them.", "Try it out!"],
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
